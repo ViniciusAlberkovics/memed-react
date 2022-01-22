783093e1f5d347e4f7ef20b8e7203c88e39b3eee
Vinicius Alberkovics <alberkovicsvinicius@gmail.com>
2022-01-21 22:12:34 -0300

    feat: new properties patient

--- src/domain/Patient.ts
+++ src/domain/Patient.ts
@@ -1,9 +1,15 @@
+/* eslint-disable camelcase */
 export interface Patient {
+  idExterno: string
+  cpf: string
   nome: string
+  data_nascimento: string
   endereco: string
   cidade: string
   telefone: string
+  nome_social?: string
+  nome_mae?: string
+  dificuldade_locomocao?: boolean
   peso?: number
   altura?: number
-  idExterno: string
 }
